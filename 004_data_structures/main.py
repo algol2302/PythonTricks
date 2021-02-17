a48fc28d4cf3b26ea06efdbc645e11a20b916fc7
Alexander Golovnya <algol.mail@gmail.com>
2021-02-17 12:21:52 +0700

    Add some interesing datastructures

--- 004_data_structures/main.py
+++ 004_data_structures/main.py
@@ -1,4 +1,8 @@
 from types import MappingProxyType
+from collections import namedtuple
+from typing import NamedTuple
+from sys import getsizeof
+from struct import Struct
 
 
 def test_MappingProxyType(name):
@@ -22,9 +26,46 @@ def test_bytes():
     print(ba)
 
 
+def test_namedtuples():
+    simple_car = ('red', 123)
+    print(f"Repr: {simple_car}, size: {getsizeof(simple_car)}")
+
+    Car1 = namedtuple('Auto', 'color mileage')
+    car1 = Car1('red', 123)
+    print(
+        f"Repr: {car1}, "
+        f"properties: {car1.color, car1.mileage}, "
+        f"size: {getsizeof(car1)}"
+    )
+
+    class Car(NamedTuple):
+        color: str
+        mileage: int
+
+    car2 = Car(color='red', mileage=123)
+    print(
+        f"Repr: {car2}, "
+        f"properties: {car2.color, car2.mileage}, "
+        f"size: {getsizeof(car2)}"
+    )
+
+
+def test_Struct():
+    """https://docs.python.org/3/library/struct.html"""
+
+    MyStruct = Struct('i?f')
+    data = MyStruct.pack(123, True, 42.1)
+    print(f"Packed data: {data}, unpacked: {MyStruct.unpack(data)}")
+
+
 if __name__ == '__main__':
     test_MappingProxyType('PyCharm')
-
     print("--------")
 
     test_bytes()
+    print("--------")
+
+    test_namedtuples()
+    print("--------")
+
+    test_Struct()
